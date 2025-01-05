94e049757be2c39128ea4d5023e5c573ee1b98f1
David Polach <d.polach@dpolach.com>
2025-01-05 23:30:27 +0100

    Initial version of MemberGroup logic

--- backend/src/main/java/club/klabis/domain/appusers/ApplicationGrant.java
+++ backend/src/main/java/club/klabis/domain/appusers/ApplicationGrant.java
@@ -5,18 +5,25 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public enum ApplicationGrant {
-    MEMBERS_REGISTER("Může registrovat nové členy klubu", true),
-    MEMBERS_EDIT("Může editovat jiné členy klubu", true),
-    MEMBERS_SUSPENDMEMBERSHIP("Může suspendovat členství v klubu", true),
-    APPUSERS_PERMISSIONS("Může spravovat oprávnění v aplikaci", true);
+    MEMBERS_REGISTER("Může registrovat nové členy klubu", true, false, false),
+    MEMBERS_EDIT("Může editovat jiné členy klubu", true, false, false),
+    MEMBERS_SUSPENDMEMBERSHIP("Může suspendovat členství v klubu", true, false, false),
+    APPUSERS_PERMISSIONS("Může spravovat oprávnění v aplikaci", true, false, false);
 
     private final String description;
     // tells if grant represents permission which can be valid across whole application (= can be set to member and then it's valid globally). Use `false` when grant validity is valid only for selected members (must be configured together with scope - either specific member(s) or group(s))
     private final boolean globalGrant;
+    // tells if grant can be used as member scoped grant
+    private final boolean memberScopedGrant;
+    // tells if grant can be used as member group scoped grant
+    private final boolean groupScopedGrant;
 
-    ApplicationGrant(String description, boolean globalGrant) {
+
+    ApplicationGrant(String description, boolean globalGrant, boolean memberScopedGrant, boolean groupScopedGrant) {
         this.description = description;
         this.globalGrant = globalGrant;
+        this.groupScopedGrant = groupScopedGrant;
+        this.memberScopedGrant = memberScopedGrant;
     }
 
     public String getDescription() {
@@ -25,6 +32,14 @@ public enum ApplicationGrant {
 
     private boolean isGlobalGrant() {
         return globalGrant;
+    }
+
+    public boolean isGroupScopedGrant() {
+        return groupScopedGrant;
+    }
+
+    public boolean isMemberScopedGrant() {
+        return memberScopedGrant;
     }
 
     public static Set<ApplicationGrant> globalGrants() {
